bdd3ea995d52378945980b899a0f48260583d44f
Ivo Woltring <ivonet@gmail.com>
2017-10-22 21:28:41 +0200

    clean

--- clean.sh
+++ clean.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 rm -rf ./test-data/ftp/admin/.camel
+rm -rf ./test-data/ftp/admin/*
 rm -rf ./test-data/ftp/user/.camel
-rm -rf ./test-data/SimpleJmsRoute/.camel
+rm -rf ./test-data/JmsRoute/.camel
 rm -rf ./test-data/startingPoint/.camel
 rm -rf ./test-data/eip/message_routing/recipient_list
-rm -f ./target/FtpToFileRoute/*
-rm -f ./target/MessageTranslatorUsingProcessor/*
-rm -f ./target/MessageTranslatorUsingBean/*
-rm -f ./target/MessageTranslatorUsingTransform/*
+rm -f ./target/xml/*
+rm -f ./target/csv/*
+rm -f ./target/json/*
+rm -f ./target/txt/*
