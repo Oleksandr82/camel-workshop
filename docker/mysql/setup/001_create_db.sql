eb97c5204eccc74d708a641777790f607e05c5bd
Ivo Woltring <ivonet@gmail.com>
2017-10-25 18:29:00 +0200

    removed all except for the readme and basic stuff

--- docker/mysql/setup/001_create_db.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE `camel_workshop` CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON camel_workshop.* To 'admin'@'%' IDENTIFIED BY 'secret';
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-./clean.sh
-docker-compose up -d
-mvn clean package spring-boot:run
